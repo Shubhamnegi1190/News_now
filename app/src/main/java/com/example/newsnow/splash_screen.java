b03402b0dc9fbca9abee3be5399b46787dbb3c5c
Shubham <negih2126@gmail.com>
2024-01-31 09:41:25 +0530

    Final commit

---
+++ app/src/main/java/com/example/newsnow/splash_screen.java
@@ -0,0 +1,29 @@
+package com.example.newsnow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+  Handler handler = new Handler();
+    handler.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            startActivity(new Intent (splash_screen.this, MainActivity.class));
+            finish();
+
+
+
+
+        }
+    },4000);
+
+    }
+}
